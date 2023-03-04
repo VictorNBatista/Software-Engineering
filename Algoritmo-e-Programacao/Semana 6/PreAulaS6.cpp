85e81425af9b68bf8dc5b498a6b4bdb55a6be41b
VictorNBatista <100983454+VictorNBatista@users.noreply.github.com>
2023-03-04 04:40:40 -0300

    Add files via upload

---
+++ Algoritmo-e-Programacao/Semana 6/PreAulaS6.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<math.h>
+#include<locale.h>
+
+main()
+{
+setlocale(LC_ALL,"Portuguese");
+float a, b, c, media;
+printf("Digite a primeira nota: ");
+scanf("%f", &a);
+printf("Digite a segunda nota: ");
+scanf("%f", &b);
+printf("Digite a terceira nota: ");
+scanf("%f", &c);
+
+media = ((a+b+c)/3);
+printf("\n\t Sua média é de: %.1f", media);
+
+if (media<40)
+	printf("\n\t Você está reprovado '-'");
+else if (media>39 && media<60)
+{
+	printf("\n\t Você deverá fazer o exame final");
+	printf("\n\t Faltam %.2f para a média", 60-media);
+}
+else	
+	printf("\n\t Você está  aprovado :)");
+}
+
